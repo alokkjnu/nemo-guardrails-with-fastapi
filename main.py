7874ea70fb71a2534de3c9c97d8d55f85b6de502
Alok Kumar Maurya <ry7069577@gmail.com>
2026-04-30 17:44:38 +0530

    nemo guardrails added

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ load_dotenv()
 app = FastAPI()
 
 #configure guardrails env variables manually or use .env file
+config_path = Path(__file__).parent / "guardrails_config.yaml"
+config = RailsConfig.from_path(str(config_path))
 
+if config.models and len(config.models) > 0:
+    model_config = config.models[0]
+    model_config.parameteres['azure_endpoint'] = os.getenv("AZURE_OPENAI_ENDPOINT")
+    model_config.parameteres['azure_deployment'] = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+    model_config.parameteres['azure_version'] = os.getenv("AZURE_OPENAI_VERSION")
+    model_config.parameteres['azure_key'] = os.getenv("AZURE_OPENAI_KEY ")
 
+rails = LLMRails(config)
 class InputData(BaseModel):
     input: str
 
@@ -23,6 +32,16 @@ async def read_root():
 @app.post("/process")
 async def process_input(data:InputData):
     input_text = data.input
+    nemo_response = await rails.generate_asyn(
+        messages = [{"role": "user", "content": input_text}]
+    )
+    nemo_output = nemo_response["content"]
+
+    if "refuse" in nemo_output.lower():
+        return {"error": "Input was refused by guardrails."}
+    
+    if "no" in nemo_output.lower():
+        return {"error": "Input was rejected by guardrails."}
 
     return {"processed_input": input_text}
 
